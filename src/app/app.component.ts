e91373f036ad79f21aa949e66f312c6c9f6d7330
Ghazwan Aliesh <ghazwan.aliesh@gmail.com>
2018-09-02 09:31:30 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { GithubService } from './shared/services/github.service';
 import { Component } from '@angular/core';
+import { GithubItem } from './shared/models/github.response';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gitbitlab';
+  query;
+  items: GithubItem[];
+
+  constructor(private github: GithubService) {}
+
+  ngOnInit() {}
+
+  handleQuery(e) {
+    if (e.keyCode == 13) {
+      this.github
+        .getRepos(this.query, '')
+        .subscribe(res => (this.items = res.items));
+    }
+  }
+
+  copy(url) {
+    const txt = document.createElement('textarea');
+    document.body.appendChild(txt);
+    txt.value = `git pull ${url}`; // chrome uses this
+    txt.textContent = `git pull ${url}`; // FF uses this
+    var sel = getSelection();
+    var range = document.createRange();
+    range.selectNode(txt);
+    sel.removeAllRanges();
+    sel.addRange(range);
+    if (document.execCommand('copy')) {
+      console.log('copied');
+    }
+    document.body.removeChild(txt);
+  }
+
+  filterQuery(e: KeyboardEvent) {
+    console.log(e.target['value']);
+    if (e.keyCode == 13) {
+      this.github
+        .getRepos(this.query, e.target['value'])
+        .subscribe(res => (this.items = res.items));
+    }
+  }
 }
